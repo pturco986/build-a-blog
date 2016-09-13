4891130ef37f0ecc47b46f5b61e5a8ec140363fc
Patrick <pturco986@gmail.com>
2016-09-12 20:29:23 -0500

    Second commit of project

--- main.py
+++ main.py
@@ -1,25 +1,63 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import webapp2
+import jinja2
+import os
+from google.appengine.ext import db
 
-class MainHandler(webapp2.RequestHandler):
+#set up jinja
+template_dir = os.path.join(os.path.dirname(__file__), "templates")
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                                autoescape = True)
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class BlogPost(db.Model):
+    title = db.StringProperty(required = True)
+    blogtext = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
+
+class MainPage(Handler):
+    """For storing blogposts in the database and inputting new ones"""
+    def render_base(self, title="", blogtext="", error=""):
+        blogs = db.GqlQuery("SELECT * FROM BlogPost"
+                            "ORDER BY created DESC")
+
+        self.render("base.html", title=title, blogtext=blogtext, error=error, blogs = blogs)
+
     def get(self):
-        self.response.write('Hello world!')
+        self.render_base()
+
+    def post(self):
+        title = self.request.get("title")
+        blogtext = self.request.get("blogtext")
+
+        if title and blogtext:
+            b = BlogPost(title = title, blogtext = blogtext)
+            b.put()
+
+            self.redirect("/")
+        else:
+            error = "We need both a title and some content!"
+            self.render_base(title, blogtext, error)
+
+    # def post(self):
+    #     self.response.write(self.request.POST['blogtext'])
+    #
+
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainPage),
+    ('/blog', MainPage),
+    ('/newpost', MainPage)
+
 ], debug=True)
